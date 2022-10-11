d4545aae226a53f1749ba29908e0dcbb4a8497fc
Gerardo Suarez <gasb47@gmail.com>
2022-10-10 21:36:45 -0300

    add speed dial

--- src/views/PintaPizarra.jsx
+++ src/views/PintaPizarra.jsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import SpeedDial from "../components/SpeedDial";
 import { Box } from "@mui/system";
 
 
@@ -6,6 +8,7 @@ function PintaPizarra() {
     return (
       <Box>
 <h1>Pinta Pizarra</h1>
+<SpeedDial></SpeedDial>
       </Box>
   
     );

096dbdab7548bf54b88867bdc3e4c908e8bf7cee
Gerardo Suarez <gasb47@gmail.com>
2022-11-21 16:16:13 -0300

    add morty task card api

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,40 +1,49 @@
 import { Button, CardHeader, Avatar, IconButton } from "@mui/material";
 import { Container } from "@mui/system";
+import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
-
-
+import { avatarMorty } from "../funciones/api";
 
 function TaskCard({ task }) {
   const { deleteTask } = useContext(TaskContext);
+  const [personajes, setPersonajes] = useState(null);
+
+  useEffect(() => {
+    avatarMorty(setPersonajes);
+  }, []);
 
   return (
     <Container>
       <CardHeader
         avatar={
           <Avatar aria-label="">
-          <Avatar src={'https://robohash.org/' + task.id + 10}/>
+            <Avatar src={"https://robohash.org/" + task.id + 10} />
           </Avatar>
         }
-        action={
-          <IconButton aria-label="">
-            
-          </IconButton>
-        }
+        action={<IconButton aria-label=""></IconButton>}
         title=""
         subheader=""
-        
       />
       <h1>{task.title}</h1>
       <p>{task.description}</p>
-      <Button sx={{m: 3}}
+      <Button
+        sx={{ m: 3 }}
         variant="contained"
         color="error"
         onClick={() => deleteTask(task.id)}
       >
         Eliminar Tarea
       </Button>
-      
+      {personajes !== null
+        ? personajes
+            .filter((p) => p.id == task.id)
+            .map((personajes) => (
+              <div key={personajes.id}>
+                <img src={personajes.image} alt="" />
+              </div>
+            ))
+        : "no hay personajes"}
     </Container>
   );
 }
---
+++ src/funciones/api.jsx
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const avatarMorty = async (state) => {
+    const peticion = await axios.get("https://rickandmortyapi.com/api/character")
+    state(peticion.data.results)
+    
+}
+
+export {
+    avatarMorty
+}
+

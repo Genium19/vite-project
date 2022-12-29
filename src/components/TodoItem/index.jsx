92f74c3d5f9410feba745d1148229feeb382256d
Gerardo Suarez <gasb47@gmail.com>
2022-12-28 21:45:08 -0300

    Edit text todo

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import './TodoItem.css';
+import React from "react";
+import { EditText, EditTextarea } from "react-edit-text";
+import "./TodoItem.css";
 
 function TodoItem(props) {
   return (
-    <li className={`TodoItem ${props.completed && 'TodoItem-check'}`}>
+    <li className={`TodoItem ${props.completed && "TodoItem-check"}`}>
       <span
-        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
         onClick={props.onComplete}
       >
         O
       </span>
-      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
-        {props.text}
-      </p>
-      <span
-        className="Icon Icon-delete"
-        onClick={props.onDelete}
+
+      <EditTextarea
+        className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}
+        defaultValue={props.text}
+        
       >
+        {props.text}
+      </EditTextarea>
+      <span className="Icon Icon-delete" onClick={props.onDelete}>
         X
       </span>
+      {/* <EditText
+        className="TodoItem"
+        name="textbox3"
+        defaultValue={props.text}
+        editButtonProps={{ style: { marginLeft: "5px", width: 16 } }}
+        showEditButton
+      /> */}
     </li>
   );
 }

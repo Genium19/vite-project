838557f1ca0310598e280e03bbc90d36b57959e2
Gerardo Suarez <gasb47@gmail.com>
2023-07-20 23:49:01 -0300

    version home preliminar finalizada

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,15 +6,16 @@ import {
   LogoNodejs,
   IconDark,
   IconLight,
+  LogoMario
 } from "../Curriculo/Curriculoicon";
 
 function Home() {
   return (
-    <div className="ml-16">
+    <div className="ml-14">
       <header className="p-0 m-0">
-        <nav className="flex bg-primary justify-end py-4 sm:px-12 xs:px-6">
+        <nav className="flex bg-primary fixed top-0 left-0 w-full justify-end items-end pb-4 h-[120px] sm:pr-12 xs:pr-6 z-50">
           {/* Contenido del menú de navegación */}
-          <ul className="space-x-3 font-regular text-base xs:text-sm sm:text-sm md:text-lg lg:text-xl">
+          <ul className="space-x-3 font-regular xs:text-sm md:text-lg">
             <li>
               <a href="#" className="text-white">
                 Inicio
@@ -35,62 +36,59 @@ function Home() {
       </header>
 
       <main className="p-0 m-0">
-        <section className="hero text-center p-6 m-0 grid xs:grid-cols-1 sm:grid-cols-2 items-center gap-4 bg-blue-100">
+        <section className="hero relative bg-imghero bg-cover py-40 text-center m-0 grid xs:grid-cols-1 sm:grid-cols-2 items-center gap-4">
           {/* Contenido del hero banner */}
-          <div className="hero--izq">
-            <h1 className="font-Unbounded text-primary xs:text-xl sm:text-2xl lg:text-4xl">
+          <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+          <div className="hero--izq relative">
+            <h1 className="font-Unbounded text-white xs:text-xl sm:text-2xl lg:text-4xl">
               Bienvenidos a Genium App{" "}
             </h1>
-            <p className="mt-4 xs:text-lg lg:text-xl text-gray-600">
+            <p className="mt-4 xs:text-lg lg:text-xl text-white">
               En esta aplicación verás algunos proyectos que he realizado
             </p>
-            <button className="mt-4 bg-blue-500 hover:bg-primary text-white font-bold py-2 px-4 rounded-full mx-auto block">
+            <button className="mt-4 bg-blue-500 hover:bg-primary text-white py-2 px-4 rounded-full mx-auto block">
               Ver Curriculo
             </button>
           </div>
-          <div className="hero--der flex justify-center">
-            <img
-              className="rounded-2xl w-4/6 h-auto m-8"
-              src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
-              alt="Imagen de ejemplo"
-            />
+          <div className="hero--der relative w-28 mx-auto transition-transform hover:scale-x-[-1]">
+            <LogoMario></LogoMario>
           </div>
         </section>
-        <div className="h-10 bg-blue-300 rounded-b-full"></div>
+        <div className="h-5 bg-blue-300 border-2 border-dashed border-opacity-20 border-blue-950 rounded-b-full"></div>
 
-        <section className="products py-12">
-          <h2 className="text-2xl text-center mb-6">Proyectos Realizados</h2>
-          <div className="grid xs:mx-0 xs:px-4 sm:mx-4 sm:px-8 md:px-20 gap-6 xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-white">
-            <div className="product relative bg-imgpokomonos bg-cover rounded-3xl p-10">
+        <section className="products">
+          <h2 className="font-Unbounded text-primary xs:text-lg sm:text-xl lg:text-4xltext-2xl text-center my-16">Proyectos Realizados</h2>
+          <div className="grid xs:mx-4 xs:px-1 md:mx-16 gap-6 xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-white">
+            <div className="product relative bg-imgpokomonos bg-cover rounded-3xl p-10 transition-transform hover:scale-105">
             <div className="absolute inset-0 bg-primary bg-opacity-70 rounded-3xl"></div>
               <h3 className="relative text-lg text-center font-semibold mt-2">
                 Pokomonos
               </h3>
               <p className="relative text-center">Game Developer</p>
-              <button className="relative bg-blue-500 hover:bg-primary text-white font-bold py-2 px-4 rounded mt-2 mx-auto block">
+              <button className="relative w-36 rounded-full bg-blue-500 hover:bg-primary text-white py-2 px-4 mt-2 mx-auto block">
                 Jugar
               </button>
 
             </div>
 
-            <div className="product relative bg-imgtodo bg-cover bg-center rounded-3xl p-10">
+            <div className="product relative bg-imgtodo bg-cover bg-center rounded-3xl p-10 transition-transform hover:scale-105">
             <div className="absolute inset-0 bg-primary bg-opacity-70 rounded-3xl"></div>
               <h3 className="relative text-lg text-center font-semibold mt-2">
                 TO DO Machine
               </h3>
               <p className="relative text-center">Organiza tus tareas</p>
-              <button className="relative bg-blue-500 hover:bg-primary text-white font-bold py-2 px-4 rounded mt-2 mx-auto block">
+              <button className="relative w-36 rounded-full bg-blue-500 hover:bg-primary text-white py-2 px-4 mt-2 mx-auto block">
                 Comenzar
               </button>
 
             </div>
-            <div className="product relative bg-imgtodo bg-cover bg-center rounded-3xl p-10">
+            <div className="product relative bg-imgtodo bg-cover bg-center rounded-3xl p-10 transition-transform hover:scale-105">
             <div className="absolute inset-0 bg-primary bg-opacity-70 rounded-3xl p-10"></div>
               <h3 className="relative text-lg text-center font-semibold mt-2">
                 Pinta Pizarra
               </h3>
               <p className="relative text-center">Eres un artista en potencia</p>
-              <button className="relative bg-blue-500 hover:bg-primary text-white font-bold py-2 px-4 rounded mt-2 mx-auto block">
+              <button className="relative w-36 rounded-full bg-blue-500 hover:bg-primary text-white py-2 px-4 mt-2 mx-auto block">
                 Dar pinceladas
               </button>
   
@@ -98,11 +96,11 @@ function Home() {
           </div>
         </section>
 
-        <section className="featured p-16">
-          <h2 className="text-2xl text-center mb-6">Tecnologías Utilizadas</h2>
-          <div className="p-0 m-0 grid grid-cols-3 grid-rows-1 gap-16">
-            <div className="feature flex flex-col items-center justify-start ">
-              <div className="flex flex-col justify-center h-52 w-32">
+        <section className="featured xs:px-6 sm:px-16 mb-32">
+          <h2 className="font-Unbounded text-primary xs:text-lg sm:text-xl lg:text-4xltext-2xl text-center my-16">Tecnologías Utilizadas</h2>
+          <div className="flex flex-wrap gap-16">
+            <div className="feature flex-1 p-6 rounded-3xl border-b-2 border-solid border-sky-400 min-w-[200px] shadow-none transition-shadow hover:shadow-xl">
+              <div className="w-28 mx-auto">
                 <LogoReact></LogoReact>
               </div>
               <h3 className="text-lg text-center font-semibold my-4">React</h3>
@@ -114,8 +112,8 @@ function Home() {
                 optimizar el rendimiento de las aplicaciones web.
               </p>
             </div>
-            <div className="feature flex flex-col items-center justify-start">
-              <div className="flex flex-col justify-center h-52 w-32">
+            <div className="feature flex-1 p-6 rounded-3xl border-b-2 border-solid border-sky-400 min-w-[200px] shadow-none transition-shadow hover:shadow-xl">
+              <div className="w-24 mx-auto">
                 <LogoJs></LogoJs>
               </div>
               <h3 className="text-lg text-center font-semibold my-4">
@@ -130,8 +128,8 @@ function Home() {
                 interacción con el usuario, llamadas a API y más.
               </p>
             </div>
-            <div className="feature flex flex-col items-center justify-start">
-              <div className="flex flex-col justify-center h-52 w-32">
+            <div className="feature flex-1 p-6 rounded-3xl border-b-2 border-solid border-sky-400 min-w-[200px] shadow-none transition-shadow hover:shadow-xl">
+              <div className="w-28 mx-auto">
                 <LogoNodejs></LogoNodejs>
               </div>
               <h3 className="text-lg text-center font-semibold my-4">
@@ -151,7 +149,7 @@ function Home() {
           </div>
         </section>
 
-        <section className="cta text-center">
+        <section className="cta text-center mb-10">
           {/* Contenido del llamado a la acción (call-to-action) */}
           <h2>
             "El conocimiento es un tesoro que seguirá a su dueño a cualquier
@@ -160,7 +158,7 @@ function Home() {
         </section>
       </main>
 
-      <footer className="bg-primary">
+      <footer className="bg-primary h-16 flex items-center justify-center">
         {/* Contenido del pie de página */}
         <p>Todos los zurdos reservados &copy; 2023</p>
       </footer>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
 
       backgroundImage:{
+        'imghero': "url(https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80)",
         'imgpokomonos': "url(https://images.unsplash.com/photo-1613771404721-1f92d799e49f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1738&q=80)",
         'imgtodo': "url(https://plus.unsplash.com/premium_photo-1683309568772-57011d6c1b7b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80)"
       },
@@ -26,7 +27,7 @@ module.exports = {
       Unbounded: ['Unbounded', 'cursive']
       },
       screens: {
-        'xs': '320px',
+        'xs': '300px',
       },
 
     },
